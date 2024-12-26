e4e83e2110fd0fde6fb62b31878f1561196cb2a2
Kaustubh Patil <patilkaustubh990@gmail.com>
2024-12-26 21:35:40 +0530

    Update Firebase configuration to use environment variables and add .env to .gitignore

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIHt4YpV8Beraa6bsuUflyjErx75_EVXo",
-  authDomain: "kanban-74300.firebaseapp.com",
-  projectId: "kanban-74300",
-  storageBucket: "kanban-74300.firebasestorage.app",
-  messagingSenderId: "364257884711",
-  appId: "1:364257884711:web:7f31dab8f92344eb0eec02"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
